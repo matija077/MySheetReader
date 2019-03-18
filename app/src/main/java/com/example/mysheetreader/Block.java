fcc3a7158f52d17b94830e9447ad4aa34a0672b8
Matija077 <matija.prs@gmail.com>
2019-03-18 15:32:18 +0100

    add data class "Block"

---
+++ app/src/main/java/com/example/mysheetreader/Block.java
@@ -0,0 +1,114 @@
+package com.example.mysheetreader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Block {
+	private List<Category> categories;
+	
+	public Block() {
+		this.categories = new ArrayList<>();
+	}
+
+	public void createCategory(String name) {
+		Category category = new Category(name);
+		categories.add(category);
+	}
+
+	public Category getCategory(int position) {
+		return categories.get(position);
+	}
+
+	public void addRow(int categoryPosition, String subCategory, String data) {
+		Category category = this.getCategory(categoryPosition);
+		category.createRow(subCategory, data);
+	}
+
+	public Category.Row getRow(int categoryPosition, int rowPosition) {
+		return this.getCategory(categoryPosition).getRow(rowPosition);
+	}
+
+	public void addData(String newData, int categoryPosition, int rowPosition) {
+		Category category = categories.get(categoryPosition);
+		category.addData(newData, rowPosition);
+	}
+
+	public Integer getDataInteger(int categoryPosition, int rowPosotion) {
+		return getCategory(categoryPosition).getDataInteger(rowPosotion);
+	}
+
+	public String getData(int categoryPosition, int rowPosotion) {
+		return getCategory(categoryPosition).getData(rowPosotion);
+	}
+
+
+	private class Category {
+		private String name;
+		private List<Row> rows;
+
+		private Category(String name) {
+			this.name = name;
+			this.rows = new ArrayList<>();
+		}
+
+		private void createRow(String subCategory, String data) {
+			Row row = new Row(subCategory, data);
+			rows.add(row);
+		}
+
+		private Row getRow(int position){
+			return rows.get(position);
+		}
+
+		private void addData(String newdata, int rowPosition) {
+			Row row = getRow(rowPosition);
+			row.addData(newdata);
+		}
+
+		private Integer getDataInteger(int rowPosition){
+			return getRow(rowPosition).getDataInteger();
+		}
+
+		private String getData(int rowPosition){
+			return getRow(rowPosition).getData();
+		}
+
+		private class Row {
+			private String subCategory;
+			private String data;
+			private Integer dataInteger;
+
+			private Row(String subCategory, String data) {
+				this.subCategory = subCategory;
+				this.data = data;
+				this.dataInteger = 0;
+				createDataInteger(data);
+			}
+
+			private void addData(String newData) {
+				this.data += "+" + newData;
+				addDataInteger(newData);
+			}
+
+			private void addDataInteger(String newdata) {
+				this.dataInteger += Integer.valueOf(newdata);
+			}
+
+			private void createDataInteger(String data) {
+				String[] subStrings = data.split("\\+");
+				for (String substring:subStrings) {
+					this.dataInteger += Integer.valueOf(substring);
+				}
+			}
+
+			private Integer getDataInteger() {
+				return this.dataInteger;
+			}
+
+			private String getData() {
+				return this.data;
+			}
+		}
+	}
+}
